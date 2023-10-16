0c0ce2440d895bf3cbb28bf0542cf62e8261530d
Son <>
2023-10-16 15:22:57 +0700

    JSON encode

--- src/model/user.go
+++ src/model/user.go
@@ -5,13 +5,13 @@ import (
 )
 
 type User struct {
-	ID        int
-	Phone     string
-	Name      string
-	Birthdate time.Time
-	AvatarURL string
-	Email     string
-	Balance   int
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int       `json:"ID"`
+	Phone     string    `json:"phone"`
+	Name      string    `json:"name"`
+	Birthdate time.Time `json:"birthdate"`
+	AvatarURL string    `json:"avatarURL"`
+	Email     string    `json:"email"`
+	Balance   int       `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
